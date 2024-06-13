e774fde05fb222cb091dd05aabee24dd345e07df
David Reverter Valeiras <d.r.valeiras@gmail.com>
2024-06-13 11:18:51 +0200

    Improves responsiveness

--- app/ui/Title.tsx
+++ app/ui/Title.tsx
@@ -7,8 +7,8 @@ const infoText =
 const Title: React.FC = () => {
   return (
     <div className="flex flex-col items-center gap-4 text-center mt-6 nb-4 md:my-8">
-      <h1 className="text-5xl md:text-6xl font-bold">Working Day</h1>
-      <p className="text-lg md:text-xl w-3/4 font-light">
+      <h1 className="text-4xl min-[420px]:text-5xl md:text-6xl font-bold">Working Day</h1>
+      <p className="text-base min-[420px]:text-lg md:text-xl w-3/4 font-light">
         A time tracker app to manage your work hours
         <Info text={infoText} className="pb-2 ml-1" />
       </p>
